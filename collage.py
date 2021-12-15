8d44b9ce3b0bb77be2db96c8b1fb48d3f85719ff
andreitr <andrei.taraschuk@gmail.com>
2021-12-14 22:52:46 -0700

    Added multiple output options.

--- collage.py
+++ collage.py
@@ -3,19 +3,18 @@ import urllib.request, json
 import random
 
 # OpenSea collection slug
-os_slug = "noadz"
+os_slug = "mistletoadz"
 
 # Number of pages to sample. Default is 2 which is about 100 images.
 # You can increase this number for more variability
-os_sample = 2
+os_sample = 5
 
-collage = Image.new("RGBA", (1500, 500), color=(255,255,255,255))
 images = []
 
 img_idx = 0
 for i in range(os_sample):
   # Pagination offset
-  offset = i*50
+  offset = 500 + (i*50)
 
   # Load collection data
   with urllib.request.urlopen("https://api.opensea.io/api/v1/assets?order_direction=desc&offset=%s&limit=50&collection=%s" % (offset,os_slug)) as url:
@@ -34,13 +33,68 @@ random.shuffle(images)
 img_idx = 0
 
 # Create 1500x500 collage and open the resulting image
+collage_xs = Image.new("RGBA", (1500, 500), color=(255,255,255,255))
 for i in range(15):
   for j in range(5):
     
     file = images[img_idx]
     img = Image.open(file).convert("RGBA")
     img = img.resize((100, 100))
-    collage.paste(img, (i * 100, j * 100))
+    collage_xs.paste(img, (i * 100, j * 100))
     img_idx +=1 
-  
-collage.show()
+
+# Create 1500x500 collage and open the resulting image
+img_idx = 0
+random.shuffle(images)
+collage_sm = Image.new("RGBA", (1500, 500), color=(255,255,255,255))
+for i in range(12):
+  for j in range(4):
+
+    file = images[img_idx]
+    img = Image.open(file).convert("RGBA")
+    img = img.resize((125, 125))
+    collage_sm.paste(img, (i * 125, j * 125))
+    img_idx +=1
+
+
+# Create 1500x500 collage and open the resulting image
+img_idx = 0
+random.shuffle(images)
+collage_sm = Image.new("RGBA", (1500, 500), color=(255,255,255,255))
+for i in range(12):
+  for j in range(4):
+
+    file = images[img_idx]
+    img = Image.open(file).convert("RGBA")
+    img = img.resize((125, 125))
+    collage_sm.paste(img, (i * 125, j * 125))
+    img_idx +=1
+
+img_idx = 0
+random.shuffle(images)
+collage_md = Image.new("RGBA", (1500, 500), color=(255,255,255,255))
+for i in range(6):
+  for j in range(2):
+
+    file = images[img_idx]
+    img = Image.open(file).convert("RGBA")
+    img = img.resize((250, 250))
+    collage_md.paste(img, (i * 250, j * 250))
+    img_idx +=1
+
+img_idx = 0
+random.shuffle(images)
+collage_lg = Image.new("RGBA", (1500, 500), color=(255,255,255,255))
+for i in range(3):
+  for j in range(1):
+
+    file = images[img_idx]
+    img = Image.open(file).convert("RGBA")
+    img = img.resize((500, 500))
+    collage_lg.paste(img, (i * 500, j * 500))
+    img_idx +=1
+
+collage_lg.show()
+collage_md.show()
+collage_sm.show()
+collage_xs.show()
